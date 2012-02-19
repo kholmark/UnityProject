9ae7e64fc004c8a074b2b2d496a1507595b87dbc
Kholmark <kholmark@hotmail.com>
2012-02-19 18:25:07 +0100

    Prueba usando TortoiseGit desde Windows.

---
+++ UnityBase/Assets/NewScript2.cs
@@ -0,0 +1,16 @@
+	// La vamos a liar parda.
+using UnityEngine;
+using System.Collections;
+
+public class NewScript2 : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
