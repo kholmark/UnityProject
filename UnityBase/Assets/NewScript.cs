79fdf4b271bae69065faef9f25f9345f32dbb4df
Juan Fernandez <kholmark@hotmail.com>
2012-02-19 15:58:20 +0100

    Modificadas escenas. Añadido NewScript.

---
+++ UnityBase/Assets/NewScript.cs
@@ -0,0 +1,16 @@
+	// No tengo ni idea de lo que estoy haciendo.
+using UnityEngine;
+using System.Collections;
+
+public class NewScript : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+	
+	}
+}
